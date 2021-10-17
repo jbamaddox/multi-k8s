bf6d1e76cb59fc4bf8323e0d3523bea41bc17093
jbamaddox <jbamaddox@gmail.com>
2021-10-16 22:44:04 -0500

    fixed error with github username

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t jbamaddox/multi-client:latest -t jbamaddox/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jbamaddox/multi-server:latest -t jbamaddox/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jbamaddox/multi-worker:latest -t jbamaddox/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push jbamaddox/multi-client:latest
-docker push jbamaddox/multi-server:latest
-docker push jbamaddox/multi-worker:latest
-docker push jbamaddox/multi-client:$SHA
-docker push jbamaddox/multi-server:$SHA
-docker push jbamaddox/multi-worker:$SHA
+docker build -t jeremybamaddox/multi-client:latest -t jeremybamaddox/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jeremybamaddox/multi-server:latest -t jeremybamaddox/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jeremybamaddox/multi-worker:latest -t jeremybamaddox/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push jeremybamaddox/multi-client:latest
+docker push jeremybamaddox/multi-server:latest
+docker push jeremybamaddox/multi-worker:latest
+docker push jeremybamaddox/multi-client:$SHA
+docker push jeremybamaddox/multi-server:$SHA
+docker push jeremybamaddox/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=jbamaddox/multi-server:$SHA
-kubectl set image deployments/client-deployment client=jbamaddox/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=jbamaddox/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=jeremybamaddox/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jeremybamaddox/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jeremybamaddox/multi-worker:$SHA
